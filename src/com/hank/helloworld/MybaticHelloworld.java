5fe2dc1c1d88f779695d58489ef3c0f8b7858877
hank <459673409@qq.com>
2016-06-04 15:09:53 +0800

    Unit测试成功，期间注意路径查找的问题 config目录必须在ClassPath下，config 右键->Build Path->Use as Source Folder， 否则mybatis报错Could not find resource SqlMapConfig.xml

--- src/com/hank/helloworld/MybaticHelloworld.java
+++ src/com/hank/helloworld/MybaticHelloworld.java
@@ -14,21 +14,27 @@ import com.hank.pojo.User;
 public class MybaticHelloworld {
 	// 根据id查询用户信息
 	@Test
-	public void findUserByIdTest() throws IOException {
+	public void findUserByIdTest() {
 		// mybatis配置文件
 		String resource = "SqlMapConfig.xml";
-		InputStream inputStream = Resources.getResourceAsStream(resource);
-		// 创建会话工厂
-		SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder()
-				.build(inputStream);
-		// 通过工厂得到SqlSession
-		SqlSession session = sqlSessionFactory.openSession();
-		// 通过SqlSession操作数据库
-		// 第一个参数：映射文件User.xml中statement的id,格式：namespace+statement的ID；
-		// 第二个参数：指定和映射文件中所指定的parameterType类型的参数
-		User user = session.selectOne("query.findUserById", 1);
-		System.out.println(user.toString());
-		//释放会话资源
+		SqlSession session = null;
+		try {
+			InputStream inputStream = Resources.getResourceAsStream(resource);
+
+			// 创建会话工厂
+			SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder()
+					.build(inputStream);
+			// 通过工厂得到SqlSession
+			session = sqlSessionFactory.openSession();
+			// 通过SqlSession操作数据库
+			// 第一个参数：映射文件User.xml中statement的id,格式：namespace+statement的ID；
+			// 第二个参数：指定和映射文件中所指定的parameterType类型的参数
+			User user = session.selectOne("query.findUserById", 1);
+			System.out.println(user.toString());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		// 释放会话资源
 		session.close();
 	}
 }
